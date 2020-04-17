7424b6df70cdc51f8f03a352b0a23a3e931c9ad7
lrg <674931583@qq.com>
2020-04-17 17:32:49 +0800

    instanceof 简单原理

---
+++ ES5/继承/instanceof.js
@@ -0,0 +1,23 @@
+function new_instance_of(leftVaule, rightVaule) { 
+    let rightProto = rightVaule.prototype; // 取右表达式的 prototype 值
+    leftVaule = leftVaule.__proto__; // 取左表达式的__proto__值
+    while (true) {
+    	if (leftVaule === null) {
+            return false;	
+        }
+        if (leftVaule === rightProto) {
+            return true;	
+        } 
+        leftVaule = leftVaule.__proto__ 
+    }
+}
+
+
+class Animal {
+    constructor(name){
+        this.name = name
+    }
+}
+
+var dog = new Animal('dog');
+console.log(new_instance_of(dog,Animal))
