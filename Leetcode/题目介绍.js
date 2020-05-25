d3b5b6a58c09302c25809043e63f1c9ac55bfc83
lrg <674931583@qq.com>
2020-05-25 14:12:13 +0800

    leetcode 62

--- Leetcode/题目介绍.js
+++ Leetcode/题目介绍.js
@@ -40,43 +40,9 @@ nums2 = [2,5,6],       n = 3
     * @return {ListNode}
     */
 
-    /**leetcode 56
-     * 
-    */
-   var merge = function(intervals) {
+/**leetcode 56
+ * 
+*/
 
-    if(intervals.length < 2) return intervals;
-
-    intervals.sort((a,b)=>(a[0]-b[0]))
-    console.log(intervals);
     
-    
-    //定义2个变量，第一个是curr一个是正再操作的变量，
-    let curr = intervals[0];
-    let result = [];
 
-    for(interval of intervals){
-        //如果当前的终止位置大于操作的起始位置，可以合并
-
-        console.log(curr[1])
-        console.log(interval[0])
-        if(curr[1] >= interval[0]){
-            //合并，取2者终止位置大的
-            console.log('zzz')
-            curr[1] = Math.max(curr[1],interval[1])
-        }else{
-
-            //小于就记录，重启第二个区间
-            result.push(curr);
-            curr = interval;
-        }
-    }
-    //如果最后的毕竟没走result.pshu，则push
-    if(curr.length !== 0 ){
-        result.push(curr)
-    }
-
-    return result
-};
-
-console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
