77aa0b97fe67acc6dc07f3bce4e18e508153bd40
lrg <674931583@qq.com>
2020-05-26 15:59:57 +0800

    忽略草稿

--- 草稿/test.js
+++ 草稿/test.js
@@ -1,5 +1,17 @@
-let str = 'channel=bglwbg/'
+let a = 'xxx1'
 
-str = str.replace('/\\/g','')
-
-console.log(str)
+function f1() {
+    console.log(this);
+    console.log(this.a);
+}
+var obj = {
+    a: "xxx2",
+    m2:function(fn){
+        fn && fn.call()
+        return function() {
+            return fn;
+        }
+    }
+}
+console.log(obj.m2(f1)());
+f1()
