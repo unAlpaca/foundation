e0403b564a68b7c902154a5014d0b17c3b51ed58
liruogu <674931583@qq.com>
2020-12-03 16:52:35 +0800

    1111

--- Dart/hello.dart
+++ Dart/hello.dart
@@ -1,10 +1,17 @@
 main(){
-    String name = "why"
-    
+    var p = Person('name');
+    print(p.age);
+    print(p.name);
 }
 
 void sayHello(String name){
     print(name);
 }
 
-void sayHello1(String name,[int])
+class Person{
+  final String name;
+  final int age;
+
+  // Person(this.name, this.age);
+  Person(this.name,{this.age = 10});
+}
